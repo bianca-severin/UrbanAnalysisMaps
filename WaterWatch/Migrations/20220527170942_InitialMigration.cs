edce27299d4842cb6a52d8bba1866e2efbfa3aa7
Bianca Severin <severin@boanet.at>
2022-05-27 19:14:37 +0200

    updated EF installation + database migration

---
+++ WaterWatch/Migrations/20220527170942_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WaterWatch.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Consumptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Neighbourhood = table.Column<string>(type: "text", nullable: false),
+                    SuburbGroup = table.Column<string>(type: "text", nullable: false),
+                    AverageMonthlyKL = table.Column<int>(type: "integer", nullable: false),
+                    Coordinates = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consumptions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consumptions");
+        }
+    }
+}
