0338990d5164ce61259f4fd86536cf18a1159f80
Bianca Severin <severin@boanet.at>
2022-05-27 18:57:38 +0200

    created Data Context Class

---
+++ WaterWatch/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WaterWatch.Models;
+
+namespace WaterWatch.Data
+{
+    public class DataContext: DbContext, IDataContext
+    {
+
+        //Constructor
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<WaterConsumption> Consumptions { get; set; }
+    }
+}
